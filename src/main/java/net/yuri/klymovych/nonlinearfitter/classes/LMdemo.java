be9764836c07618bcf000a4d509f98f3d166e514
yuryklim <yuryklim@outlook.com>
2017-11-15 20:19:08 +0200

    Adding 4 classes for Levenberg - Marquardt algorithm

---
+++ src/main/java/net/yuri/klymovych/nonlinearfitter/classes/LMdemo.java
@@ -0,0 +1,7 @@
+package net.yuri.klymovych.nonlinearfitter.classes;
+
+public class LMdemo {
+    public static void main(String args[]) {
+        new FitHost().doFit();
+    }
+}
---
+++ src/main/java/net/yuri/klymovych/nonlinearfitter/classes/LMhost.java
@@ -0,0 +1,17 @@
+package net.yuri.klymovych.nonlinearfitter.classes;
+
+public interface LMhost {
+    double dNudge(double dp[]);
+    // Allows myLM.bLMiter to modify parms[] and reevaluate.
+    // This is the only modifier of parms[].
+    // So, if NADJ<NPARMS, put your LUT here.
+
+    boolean bBuildJacobian();
+    // Allows LM to request a new Jacobian.
+
+    double dGetResid(int i);
+    // Allows LM to see one element of the resid[] vector.
+
+    double dGetJac(int i, int j);
+    // Allows LM to see one element of the Jacobian matrix.
+}
