2510489e67cfe68291fa30b794a61e11f9405dc5
yuryklim <yuryklim@outlook.com>
2017-11-15 19:15:04 +0200

    Creating maven initial project.

---
+++ src/main/java/net/yuri/klymovych/nonlinearfitter/App.java
@@ -0,0 +1,13 @@
+package net.yuri.klymovych.nonlinearfitter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
